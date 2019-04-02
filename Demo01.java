6e3ff583f560f37344cb4360d332c40b3b31b555
xiangy99 <2248421759@qq.com>
2019-04-02 15:00:32 +0800

    第三次提交

--- Demo01.java
+++ Demo01.java
@@ -1,7 +1,7 @@
-public class Demo01{
-	public static void main(String[] args){
+
+
 	
-	System.out.println("\n\n\n\t\t\t欢迎使用我行我素购物管理系统"+"\n\n* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n"+"\n\t\t\t1.客 户 信 息 管 理"+"\n\t\t\t2.购 物 结 算"+"\n\t\t\t3.真 情 回 馈"+"\n\t\t\t4.注 销"+"\n\n* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\n"+"\n清选择，输入数字");
+);
 
 	}
 
